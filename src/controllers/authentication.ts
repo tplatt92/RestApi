d2bdc5c1d83b897af8b3d4067480742a0c08520c
Tom Platt <thomasjplatt0107@gmail.com>
2024-01-28 17:01:38 +0000

    register controller created

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,43 @@
+import express from "express";
+
+import { getUserByEmail, createUser } from "../db/users";
+import { random, authentication } from "../helpers";
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    // destructure elements expected from the request as defined in the schema
+    const { email, password, username } = req.body;
+
+    // error handling if data does not exist in body
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    // get existing user
+    const existingUser = await getUserByEmail(email);
+
+    // if no user exists return error
+    if (!existingUser) {
+      return res.sendStatus(400);
+    }
+
+    // create a salt from helpers
+    const salt = random();
+
+    // call create user function passing in data from req body and adding salt to the password
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    // return success with user data
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,13 @@ app.use(
   })
 );
 
+// Compression reduces the size of data sent over the network, improving the application's performance by reducing the amount of data that needs to be transferred.
 app.use(compression());
+
+// parses incoming HTTP requests and extracts any cookies included in the request headers.
 app.use(cookieParser());
+
+// parses incoming JSON payloads from HTTP requests and makes the data available in the req.body object.
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
@@ -27,6 +32,8 @@ server.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}/`);
 });
 
+// ensures that Mongoose uses the native JavaScript promises, which are widely supported and consistent with the ECMAScript standard.
 mongoose.Promise = Promise;
+
 mongoose.connect(URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
